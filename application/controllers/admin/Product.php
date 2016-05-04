1092414b3fbeb6da2bc4b6f9889445eb51228998
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-05-04 16:54:37 +0700

    add database

--- application/controllers/admin/Product.php
+++ application/controllers/admin/Product.php
@@ -5,24 +5,34 @@ class Product extends Admin {
 
 	public function index()
 	{
-
+		redirect('/admin/product/input','refresh');
 		dd($this->products_model->get_all());
 	}
 
-	public function create(){
-		$options = [
-			"name" => "name123",
-			"description" => "123",
-			"category" => 1,
-			"price" => "100000",
-			"image" => "/asset/upload/img.jpg",
-			"active" => 1,
-			"special_content" => "",
-			"special_price" => "",
-			"views" => 0,
-		];
-		$this->products_model->insert($options);
+	public function input($id=null){
+		if($this->input->post()){
+			$options = [
+				"name"            => $this->input->post('name'),
+				"description"     => $this->input->post('description'),
+				"category"        => $this->input->post('category'),
+				"price"           => $this->input->post('price'),
+				"image"           => $this->input->post('image'),
+				"active"          => $this->input->post('active'),
+				"special_content" => $this->input->post('special_content'),
+				"special_price"   => $this->input->post('special_price'),
+				"views"           => $this->input->post('views'),
+			];
+			if($this->products_model->insert($options)){
+				$this->session->set_flashdata('alert_success', 'Đã lưu thành công');
+			}else{
+				$this->session->set_flashdata('alert_error', 'Gặp lỗi');
+			}
+			redirect('/admin/product','refresh');
+		}
+		$rs = $this->products_model->get($id);
+		$this->twig->display("admin/product/input",compact("rs"));
 	}
+
 
 }
 
