cc69f9cba1e2b5b9212814e826bd7c7093e75dc7
Son Vi Hoang <hoang_son@lampart-vn.com>
2016-05-04 15:37:48 +0700

    first commit

---
+++ application/controllers/admin/Product.php
@@ -0,0 +1,30 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Product extends CI_Controller {
+
+	public function index()
+	{
+
+		dd($this->products_model->get_all());
+	}
+
+	public function create(){
+		$options = [
+			"name" => "name123",
+			"description" => "123",
+			"category" => 1,
+			"price" => "100000",
+			"image" => "/asset/upload/img.jpg",
+			"active" => 1,
+			"special_content" => "",
+			"special_price" => "",
+			"views" => 0,
+		];
+		$this->products_model->insert($options);
+	}
+
+}
+
+/* End of file Product.php */
+/* Location: ./application/controllers/admin/Product.php */
---
+++ application/core/MY_Controller.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class MY_Controller extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+		$config = [
+			'paths' => [APPPATH.'views', VIEWPATH],
+			'cache' => APPPATH.'cache',
+		];
+		$this->load->library('twig');
+	}
+}
+
+class Admin extends MY_Controller {
+
+    public $data;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if (empty($this->session->userdata['admin_user_id']) && !in_array('login', $this->uri->segment_array())) {
+            redirect('admin/users/login');
+        }
+    }
+}
+
+class Frontend extends MY_Controller {
+
+    public $data;
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
+
+/* End of file MY_Controller.php */
+/* Location: ./application/core/MY_Controller.php */
