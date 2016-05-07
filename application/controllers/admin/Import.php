aa84f37149c457ffed90b8dc8927084d577ee091
vihoangson <vihoangson@gmail.com>
2016-05-07 09:29:29 +0700

    commit

--- application/controllers/admin/Import.php
+++ application/controllers/admin/Import.php
@@ -3,13 +3,111 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Import extends Admin {
 
+	private $array_title_table_product;
+
 	public function __construct(){
 		parent::__construct();
 		$this->load->model('product_images_model');
-		// /admin/import/product
+		$this->array_title_table_product = ["id","name","description","category","price","image","active","special_price"];
 	}
 
-	public function product()
+	public function install_category(){
+		if($this->_install_category()){
+			$this->session->set_flashdata('alert_success',"Đã install");
+		}else{
+			$this->session->set_flashdata('alert_error',"Gặp lỗi");
+		}
+		redirect('/admin/import/do_import','refresh');
+	}
+
+	/**
+	 * private function _install_category
+	 *
+	 * Function này dùng để instal default category
+	 * 
+	 * @return void
+	 * @author Santo
+	 **/
+	private function _install_category(){
+		$this->load->model('categories');
+		$this->db->query('truncate table categories');
+		$json_string = '
+		{
+			"Man":
+				{"1":"Shirts","2":"T-shirts","3":"Shoes"},
+			"Woman":
+				{"1":"Dresses","2":"Bags","3":"Rings"},
+			"Smartphones":
+				{"1":"Apple iPhone","2":"Samsung Galaxy","3":"Sony Xperia"},
+			"Tablets":
+				{"1":"Apple iPad","2":"Samsung Tablet","3":"Lenevo Tablet"},
+			"Laptop":
+				{},
+			"Desctop":
+				{},
+			"Watch":
+				{}
+		}';
+		$variable = json_decode($json_string);
+		foreach ($variable as $key => $value) {
+			$params = [
+				"name"      =>$key,
+				"parent_id" => 0
+			];
+			$id = $this->categories->insert($params);
+			foreach ($value as $key_v => $value_v) {
+				$params = [
+					"name"      =>$value_v,
+					"parent_id" => $id
+				];
+				$this->categories->insert($params);
+			}
+		}
+		return true;
+	}
+
+	public function do_export()
+	{
+		
+		$products = $this->products_model->with_product_images()->get_all();
+
+		foreach ($this->array_title_table_product as $key_t => $value_t) {
+			$array[0][]= $value_t;
+		}
+		// Add more column: path_img
+		$array[0][]= "path_img";
+
+		foreach ($products as $key => $value) {
+			// $this->array_title_table_product all off element in table products
+			foreach ($this->array_title_table_product as $key_t => $value_t) {
+				$array[$key+1][$value_t]= $value->{$value_t};
+			}
+
+			// Content for column: path_img
+			$array_img = [];
+			foreach ($value->product_images as $key_im => $value_im) {
+				$array_img[] = $value->product_images[$key_im]->path_img;
+			}
+			$array[$key+1]["path_img"]= implode(",", $array_img);
+
+		}
+		$this->_build_file_excel($array);
+	}
+	private function _build_file_excel($array){
+		$doc = new PHPExcel();
+		$doc->setActiveSheetIndex(0);
+
+		$doc->getActiveSheet()->fromArray($array, null, 'A1');
+		header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+		header('Content-Disposition: attachment;filename="your_name.xls"');
+		header('Cache-Control: max-age=0');
+
+		// Do your stuff here
+		$writer = PHPExcel_IOFactory::createWriter($doc, 'Excel5');
+		$writer->save('php://output');
+	}
+
+	public function do_import()
 	{
 		if($_FILES){
 			$path = FCPATH.'assets/import_file/';
@@ -17,12 +115,12 @@ class Import extends Admin {
 				mkdir($path);
 			}
 			$config['upload_path'] = $path;
-			$config['allowed_types'] = 'xlsx';
+			$config['allowed_types'] = 'xlsx|xls';
 			$this->load->library('upload', $config);
 			if ( ! $this->upload->do_upload()){
 				$error = array('error' => $this->upload->display_errors());
 				$this->session->set_flashdata('alert_error', "Không lưu được file");
-				redirect('/admin/import/product','refresh');
+				redirect('/admin/import/do_import','refresh');
 			}
 			else{
 				$data = array('upload_data' => $this->upload->data());
@@ -31,7 +129,7 @@ class Import extends Admin {
 			$this->db->query('truncate table product_images');
 			$db_import = $this->get_data($path.$data["upload_data"]["file_name"]);		
 			foreach ($db_import as $key => $value) {
-				foreach (["id","name","description","category","price","image","active","special_price"] as $key_t => $value_t) {
+				foreach ($this->array_title_table_product as $key_t => $value_t) {
 					$params[$value_t] = $value[$value_t];
 				}
 				$this->products_model->insert($params);
@@ -45,7 +143,7 @@ class Import extends Admin {
 				}
 			}
 			$this->session->set_flashdata('alert_success', "Import thành công");
-			redirect('/admin/import/product','refresh');
+			redirect('/admin/import/do_import','refresh');
 		}
 
 		$this->twig->display("admin/input_import");
